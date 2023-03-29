49345f437b73028daaef0afe8ef7ce75d33a1ac2
Seyi-dev66 <106405492+Seyi-dev1@users.noreply.github.com>
2023-03-29 09:13:20 +0100

    making more UI changes

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -3,9 +3,9 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import "swiper/css/pagination";
 import { Autoplay, Pagination, Navigation } from 'swiper';
-import PNG1 from '../../pngs/business-3d-bitcoin.png'
-import PNG2 from '../../pngs/bloom-girl.png'
-import PNG3 from '../../pngs/forex.png'
+// import PNG1 from '../../pngs/business-3d-bitcoin.png'
+// import PNG2 from '../../pngs/bloom-girl.png'
+// import PNG3 from '../../pngs/forex.png'
 
 const Carousel = ()=>{
     return(
@@ -15,7 +15,7 @@ const Carousel = ()=>{
         loop={true}
         centeredSlides={true}
         autoplay={{
-          delay: 4000,
+          delay: 6000,
           disableOnInteraction: false,
         }}
         // pagination={{
@@ -29,7 +29,7 @@ const Carousel = ()=>{
       >
       <SwiperSlide>
       <div>
-      <img src={PNG1} alt="png1" />
+      {/* <img src={PNG1} alt="png1" /> */}
       <h1>Experts in Assets Management</h1>
       <span>With years of experience and the talents on our team, 
       we make those returns from trading with technical 
@@ -38,18 +38,18 @@ const Carousel = ()=>{
       </SwiperSlide>
       <SwiperSlide>
       <div>
-      <img src={PNG2} alt="png2" />
+      {/* <img src={PNG2} alt="png2" /> */}
       <h1>Best Investment plans</h1>
-      <span>At Colette Rocks, 
+      <span>At Blue Horizon, 
       we provide the best investment plans to grow wealth for our clients.
        We have the best services world wide.</span>
       </div>
       </SwiperSlide>
       <SwiperSlide>
       <div>
-      <img src={PNG3} alt="png3" />
+      {/* <img src={PNG3} alt="png3" /> */}
       <h1>Registered and Insured</h1>
-      <span>Colette Rocks is a fully registered company in Delaware USA.
+      <span>Blue Horizon is a fully registered company in Delaware USA.
        We are more than qualified to provide our clients with 50% return per annum.</span>
       </div>
       </SwiperSlide>
