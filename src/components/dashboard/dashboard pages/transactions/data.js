aa2e139642343b37967f68ac93b7f0704ad0403d
Seyi-dev66 <106405492+Seyi-dev1@users.noreply.github.com>
2023-04-06 22:59:52 +0100

    editing transactions table

---
+++ src/components/dashboard/dashboard pages/transactions/data.js
@@ -0,0 +1,51 @@
+ export const columns = [
+    { field: 'id', headerName: 'Transaction Id', width: 230,  headerClassName: 'headerclass', },
+    {
+      field: 'date',
+      headerClassName: 'headerclass',
+      headerName: 'Date',
+      width: 170,
+    },
+    {
+      field: 'time',
+      headerClassName: 'headerclass',
+      headerName: 'Time',
+      width: 160,
+    },
+    {
+      field: 'category',
+      headerClassName: 'headerclass',
+      headerName: 'Category',
+      width: 190,
+    },
+    {
+      field: 'amount',
+      headerClassName: 'headerclass',
+      headerName: 'Amount',
+      width: 190,
+    },
+    {
+      field: 'status',
+      headerClassName: 'headerclass',
+      headerName: 'Status',
+      width: 150,
+    },
+  ];
+
+ export  const rows = [
+    { id: '#122351', date: '03|04|23', time:'19:30', category: 'withdrawal', amount:'$2000', status:'pending' },
+    { id: '#234575', date: '01|04|23', time:'13:00', category: 'deposit', amount:'$3000', status:'success' },
+    { id: '#243567', date: '30|03|23', time:'14:30', category: 'deposit', amount:'$2500', status:'success' },
+    { id: '#124363', date: '30|03|23', time:'14:23', category: 'withdrawal', amount:'$700', status:'success' },
+    { id: '#231426', date: '26|03|23', time:'09:47', category: 'transfer', amount:'$280', status:'declined' },
+    { id: '#1324WQ', date: '25|03|23', time:'18:17', category: 'deposit', amount:'$4200', status:'success' },
+    { id: '#64T4UH', date: '20|03|23', time:'20:51', category: 'deposit', amount:'$2000', status:'success' },
+    { id: '#WK34RT', date: '20|03|23', time:'20:30', category: 'withdrawal', amount:'$500', status:'success' },
+    { id: '#004JKQ', date: '19|03|23', time:'11:33', category: 'deposit', amount:'$7000', status:'success' },
+    { id: '#40LX3P', date: '18|04|23', time:'16:44', category: 'transfer', amount:'$360', status:'declined' },
+    { id: '#BN74S2', date: '12|03|23', time:'18:20', category: 'deposit', amount:'$1800', status:'success' },
+    { id: '#KQW362', date: '03|003|23', time:'21:09', category: 'deposit', amount:'$200', status:'success' },
+    { id: '#FF3410', date: '01|03|23', time:'15:27', category: 'withdrawal', amount:'$150', status:'success' },
+    { id: '#1RT378', date: '25|02|23', time:'17:10', category: 'deposit', amount:'$720', status:'success' },
+    { id: '#490377', date: '23|02|23', time:'09:29', category: 'deposit', amount:'$1500', status:'success' },
+  ];
--- src/components/dashboard/dashboard pages/transactions/transactionsTable.jsx
+++ src/components/dashboard/dashboard pages/transactions/transactionsTable.jsx
@@ -1,134 +1,80 @@
 import * as React from 'react';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
 import './transactionsTable.scss'
 
+export const columns = [
+  { field: 'id', headerName: 'Transaction Id', width: 230,  headerClassName: 'headerclass', },
+  {
+    field: 'date',
+    headerClassName: 'headerclass',
+    headerName: 'Date',
+    width: 170,
+  },
+  {
+    field: 'time',
+    headerClassName: 'headerclass',
+    headerName: 'Time',
+    width: 160,
+  },
+  {
+    field: 'category',
+    headerClassName: 'headerclass',
+    headerName: 'Category',
+    width: 190,
+  },
+  {
+    field: 'amount',
+    headerClassName: 'headerclass',
+    headerName: 'Amount',
+    width: 190,
+  },
+  {
+    field: 'status',
+    headerClassName: 'headerclass',
+    headerName: 'Status',
+    width: 150,
+  },
+];
 
 
-export default function TransactionsTable() {
+export  const rows = [
+  { id: '#122351', date: '03|04|23', time:'19:30', category: 'withdrawal', amount:'$2000', status:'pending' },
+  { id: '#234575', date: '01|04|23', time:'13:00', category: 'deposit', amount:'$3000', status:'success' },
+  { id: '#243567', date: '30|03|23', time:'14:30', category: 'deposit', amount:'$2500', status:'success' },
+  { id: '#124363', date: '30|03|23', time:'14:23', category: 'withdrawal', amount:'$700', status:'success' },
+  { id: '#231426', date: '26|03|23', time:'09:47', category: 'transfer', amount:'$280', status:'declined' },
+  { id: '#1324WQ', date: '25|03|23', time:'18:17', category: 'deposit', amount:'$4200', status:'success' },
+  { id: '#64T4UH', date: '20|03|23', time:'20:51', category: 'deposit', amount:'$2000', status:'success' },
+  { id: '#WK34RT', date: '20|03|23', time:'20:30', category: 'withdrawal', amount:'$500', status:'success' },
+  { id: '#004JKQ', date: '19|03|23', time:'11:33', category: 'deposit', amount:'$7000', status:'success' },
+  { id: '#40LX3P', date: '18|04|23', time:'16:44', category: 'transfer', amount:'$360', status:'declined' },
+  { id: '#BN74S2', date: '12|03|23', time:'18:20', category: 'deposit', amount:'$1800', status:'success' },
+  { id: '#KQW362', date: '03|003|23', time:'21:09', category: 'deposit', amount:'$200', status:'success' },
+  { id: '#FF3410', date: '01|03|23', time:'15:27', category: 'withdrawal', amount:'$150', status:'success' },
+  { id: '#1RT378', date: '25|02|23', time:'17:10', category: 'deposit', amount:'$720', status:'success' },
+  { id: '#490377', date: '23|02|23', time:'09:29', category: 'deposit', amount:'$1500', status:'success' },
+];
 
-  const columns = [
-    { id: 'number', label: 'Transaction Number', minWidth: 170 },
-    { id: 'date', label: 'Date', minWidth: 170 },
-    { id: 'category', label: 'Category', minWidth: 100 },
-    {
-      id: 'amount',
-      label: 'Amount',
-      minWidth: 170,
-      align: 'right',
-      format: (value) => value.toLocaleString('en-US'),
-    },
-    {
-      id: 'status',
-      label: 'Status',
-      minWidth: 170,
-      align: 'right',
-      renderCell:(params)=><div className="statusCell" style={{color:'red'}}><h1 style={{color:'red'}}>{params.row.status}</h1></div>
-    },
-  ];
-
-
-  function createData(number, date, category, amount, status) {
-    return { number, date, category, amount, status };
-  }
-
-
-
-  const rows = [
-    createData('#28456100', '18:24 020223', 'withdrawal', '$2000', 'approved' ),
-    createData('#75632102', '18:24 020223', 'transfer', '$300', 'declined'),
-    createData('#09837651', 'withdrawal', '$200', 301340),
-    createData('#99072653', 'withdrawal', '$400', 9833520),
-    createData('#30648710', 'deposit', '$300', 9984670),
-    createData('#82029941', 'deposit', '$1000', 7692024),
-    createData('#30734099', 'withdrawal', '$380', 357578),
-    createData('#31205572', 'deposit', '$550', 70273),
-    createData('#37748711', 'transfer','$50', 1972550),
-    createData('#00659463', 'withdrawal', '$250', 377973),
-    createData('#30762517', 'deposit', '$2000', 640679),
-    createData('#87730062', 'withdrawal', '$420', 242495),
-    createData('27105425', 'tramsfer', '$150', 17098246),
-    createData('#24141415', 'deposit', '$500', 923768),
-    createData('#16173245', 'deposit', '$500', 8515767),
-  ];
-
-
-
-
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
-
+export default function DataGridDemo() {
   return (
-    <div className="dataTable">
-      <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-      <TableContainer sx={{ maxHeight: 540, width:'100%' }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth, color:column.color }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                    {columns.map((column) => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === 'number'
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[5, 8, 10]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-        sx={{  width:'100%', '.MuiTablePagination-toolbar': {
-        width: 'fitContent', 
-        height: '35px',
-      },
- }}
+    <Box sx={{ height: 400, width: '100%' }}
+    rowsPerPageOptions={[8]}>
+      <DataGrid className='table'
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
+        }}
+        pageSizeOptions={[5]}
+        rowsPerPageOptions={[8]}
+        // checkboxSelection
+        disableRowSelectionOnClick
       />
-    </Paper>
-    </div>
-    
+    </Box>
   );
 }
